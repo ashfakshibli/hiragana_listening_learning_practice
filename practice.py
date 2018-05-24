33b01246a42fc5f3742f5024fc8d355cc87798ea
Ashfak Md. Shibli <shibli.emon@gmail.com>
2018-05-24 18:38:55 +0600

    Point game start

--- practice.py
+++ practice.py
@@ -4,7 +4,7 @@ def sound_play(path):
 	pygame.mixer.init()
 	pygame.mixer.music.load(path)
 	pygame.mixer.music.play()
-	get_sleep(5)
+	get_sleep(1)
 
 def get_sleep(get_time):
 	time.sleep(get_time)
@@ -25,11 +25,16 @@ for file in os.listdir(directory):
 
 
 hiragana_dict_copy = hiragana_dict
+point = 0
 while True:
 	random_hiragana = random.choice(list(hiragana_dict_copy.items()))
 	sound_play(random_hiragana[1])
-	print(random_hiragana[1])
-	break
+	answer = input("Give your answer:  ")
+	if answer is random_hiragana[0]:
+		point = point + 1
+		print("Your point in this session "+ point)
+	else:
+		continue
 
 
 
