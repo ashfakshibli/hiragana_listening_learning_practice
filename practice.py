83ab6c7987d6b417a8533296989f65f449f0e5d6
Ashfak Md. Shibli <shibli.emon@gmail.com>
2018-05-24 18:31:50 +0600

    HIragana Playing with a sleep. After playing sound sleep at least 1 second that sound plays successfully

--- practice.py
+++ practice.py
@@ -1,13 +1,13 @@
-import os, random, pygame 
-
-
-
+import os, random, pygame, time
 
 def sound_play(path):
-	# play an alarm sound
 	pygame.mixer.init()
 	pygame.mixer.music.load(path)
 	pygame.mixer.music.play()
+	get_sleep(5)
+
+def get_sleep(get_time):
+	time.sleep(get_time)
 
 
 directory = os.path.dirname(os.path.abspath(__file__))+"\sounds\\"
@@ -16,19 +16,20 @@ directory = os.path.dirname(os.path.abspath(__file__))+"\sounds\\"
 hiragana_dict = {}
 
 for file in os.listdir(directory):
-    filename = os.fsdecode(file)
-    filename_without_ext = os.path.splitext(filename)[0]
-    if filename.endswith(".mp3"): 
-        hiragana_dict[filename_without_ext] = directory+filename
-    else:
-        continue
+	filename = os.fsdecode(file)
+	filename_without_ext = os.path.splitext(filename)[0]
+	if filename.endswith(".mp3"): 
+		hiragana_dict[filename_without_ext] = directory+filename
+	else:
+		continue
 
 
 hiragana_dict_copy = hiragana_dict
 while True:
-    random_hiragana = random.choice(list(hiragana_dict_copy.items()))
-    sound_play(random_hiragana[1])
-    break
+	random_hiragana = random.choice(list(hiragana_dict_copy.items()))
+	sound_play(random_hiragana[1])
+	print(random_hiragana[1])
+	break
 
 
 
